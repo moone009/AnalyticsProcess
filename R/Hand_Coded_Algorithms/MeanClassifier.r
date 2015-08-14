146762218faa11a75560d7f05f577f104f51e211
unknown <w47593@PC78253.we.dirsrv.com>
2015-08-14 11:22:07 -0500

    Added user defined columns

--- R/Hand_Coded_Algorithms/MeanClassifier.r
+++ R/Hand_Coded_Algorithms/MeanClassifier.r
@@ -13,30 +13,31 @@
 ## Note: This algorithm assumes normally distributed data.
 
 
-MeanClassifier <- function(df,Target){
-	## Split into test and train
-	inTrain = createDataPartition(y=df[,Target],p=.7,list=FALSE)
-
-	training = df[inTrain,]
-	testing = df[-inTrain,]
-	## Calculate Means
-	training$mean = rowMeans(training[1:4])
-	testing$mean = rowMeans(testing[1:4])
-	testing$Prediction = ''
-	
-	## loop through calculate difference and assign predictions
-	for(i in 1:nrow(testing)){   
-
-	  training$Vals = abs(as.numeric(training$mean) - as.numeric(testing$mean[i]))
-	  training = training[with(training, order(Vals)), ]
-	  testing$Prediction[i] =  names(which(table(head(training[Target],5)) == max(table(head(training[Target],5)))))
-	  
-	  }
-	  
-	  return(table(testing[[Target]],testing$Prediction))
+MeanClassifier <- function(df,colls,Target){
+  ## Split into test and train
+  inTrain = createDataPartition(y=df[,Target],p=.7,list=FALSE)
+  
+  training = df[inTrain,]
+  testing = df[-inTrain,]
+  ## Calculate Means
+  training$mean = rowMeans(training[colls])
+  testing$mean = rowMeans(testing[colls])
+  testing$Prediction = ''
+  
+  ## loop through calculate difference and assign predictions
+  for(i in 1:nrow(testing)){   
+    
+    training$Vals = abs(as.numeric(training$mean) - as.numeric(testing$mean[i]))
+    training = training[with(training, order(Vals)), ]
+    #here we saying wich value in our table equals the max value of the table values then extracting the name
+    testing$Prediction[i] =  names(which(table(head(training[Target],5)) == max(table(head(training[Target],5)))))
+    
+  }
+  
+  return(table(testing[[Target]],testing$Prediction))
 }
 
-MeanClassifier(iris,'Species')
+MeanClassifier(iris,c(1,2,3,4),'Species')
 
 
 
